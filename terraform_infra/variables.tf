12a68735d7dc79a116dd6d6595532ea9218bc4e3
Kulas <randelguevarra@gmail.com>
2026-01-28 15:31:01 +0800

    Modified terraform variable

--- terraform_infra/variables.tf
+++ terraform_infra/variables.tf
@@ -4,26 +4,21 @@ variable "region" {
   description = "AWS region to deploy resources"
   type        = string
   # default     = "us-east-1"
-  # value will be injected from GitHub Actions secrets
 }
 
 variable "ami" {
   description = "AMI | us-east-1 UBUNTU"
   type        = string
   # default     = "ami-0ecb62995f68bb549"
-  # value will be injected from GitHub Actions secrets
 }
 
 variable "instance_type" {
   description = "Instance type for EC2 instances"
   type        = string
   # default     = "t3.micro"
-  # value will be injected from GitHub Actions secrets
 }
 
 variable "vpc_id" {
   description = "VPC ID where resources will be deployed"
   type        = string
-  # default     = "vpc-0b1a89f0204ae0513"
-  # value will be injected from GitHub Actions secrets
 }
